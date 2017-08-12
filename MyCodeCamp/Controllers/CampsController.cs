fa743d6c42b9c231de9f031c51d0fb7d6e4a07de
geobarteam <gvd8@hotmail.com>
2017-08-12 13:42:04 +0200

    base setup

---
+++ MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCodeCamp.Controllers
+{
+    public class CampsController: Controller
+    {
+        [HttpGet("api/[controller]")]
+        public IActionResult Get()
+        {
+            return Ok(new {Name = "Shawn", FavoriteColor = "Blue"});
+        }
+    }
+}
